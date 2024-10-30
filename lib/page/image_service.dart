b9239f7d313b4f401d1e16604f4772deafb69801
yeeun526 <yeeun4155@naver.com>
2024-10-31 00:50:40 +0900

    add logo, add loading screen

--- lib/page/image_service.dart
+++ lib/page/image_service.dart
@@ -56,8 +56,9 @@ class ImageService {
       Uri.parse('https://api.openai.com/v1/images/generations'),
       headers: {
         'Content-Type': 'application/json',
-        'Authorization':
-            'Bearer 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',
+        // !!실행할 때는 주석 풀어야 함!!
+        //'Authorization':
+        //'Bearer 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',
       },
       body: jsonEncode({'prompt': refinedPrompt}),
     );
