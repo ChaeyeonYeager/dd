56f46ecae93e545e6310cde32947941d684af987
yeeun526 <yeeun4155@naver.com>
2024-10-31 00:35:47 +0900

    add loading screen, login/register page logo

--- lib/page/loading_sceen.dart
+++ lib/page/loading_sceen.dart
@@ -5,7 +5,6 @@ class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // 실제 로딩 작업을 수행하고 완료되면 페이지를 이동
-    //_loadDataAndNavigate(context);
 
     return Scaffold(
       body: Center(
@@ -40,18 +39,6 @@ class LoadingScreen extends StatelessWidget {
       ),
     );
   }
-
-  // // 데이터 로딩과 페이지 이동을 처리하는 비동기 함수
-  // Future<void> _loadDataAndNavigate(BuildContext context) async {
-  //   // 여기서 실제 로딩 작업을 수행합니다. 예를 들어 API 호출이나 데이터베이스 쿼리 등.
-  //   await Future.delayed(Duration(seconds: 4)); // 예시로 4초 대기. 실제 작업으로 대체.
-
-  //   // 로딩 작업이 끝난 후 AuthPage로 이동
-  //   Navigator.pushReplacement(
-  //     context,
-  //     MaterialPageRoute(builder: (context) => AuthPage()),
-  //   );
-  // }
 }
 
 void showLoadingScreen(BuildContext context) {
